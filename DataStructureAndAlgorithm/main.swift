586a4e347c5b1875e43da10a09f820fb093fa0be
FattGuy <fchang@myriadmobile.com>
2017-10-30 13:11:52 -0500

    initial commit

---
+++ DataStructureAndAlgorithm/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DataStructureAndAlgorithm
+//
+//  Created by Feng Chang on 10/30/17.
+//  Copyright © 2017 Feng Chang. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
