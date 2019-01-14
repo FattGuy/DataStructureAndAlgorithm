d47d42b27f102300217caf6f5344dfeaaef77cf1
FattGuy <fchang@myriadmobile.com>
2019-01-14 09:36:09 -0600

    rewrite insertionSort

--- DataStructureAndAlgorithm/Algorithm/InsertionSort.swift
+++ DataStructureAndAlgorithm/Algorithm/InsertionSort.swift
@@ -9,35 +9,49 @@
 import Foundation
 
 class InsertionSort {
+    func insertionSort(_ array: [Int]) -> [Int] {
+        var a = array
+        for x in 1..<a.count {
+            var y = x
+            while y > 0 && a[y] < a[y - 1] {
+                a.swapAt(y - 1, y)
+                y -= 1
+            }
+        }
+        
+        return a
+    }
     
-//    func insertionSort(_ array:[Int]) -> [Int] {
-//        var a = array
-//        for x in 1..<a.count {
-//            var y = x
-//            //let temp = a[y]
-//            while y > 0 && a[y] /* temp */ < a[y - 1] {
-//                //a[y] = a[y - 1]
-//                a.swapAt(y - 1, y)
-//                y -= 1
-//            }
-//            //a[y] = temp
-//        }
-//        return a
-//    }
+    func insertionSortWithoutSwap(_ array: [Int]) -> [Int] {
+        var a = array
+        for x in 1..<a.count {
+            var y = x
+            let temp = a[y]
+            
+            while y > 0 && temp < a[y - 1] {
+                a[y] = a[y - 1]
+                y -= 1
+            }
+            
+            a[y] = temp
+        }
+        
+        return a
+    }
     
-    // The generic way
-    func insertionSort<T>(_ array: [T], _ isOrderedBefore: (T, T) -> Bool) -> [T] {
+    func insertionSorGeneric<T>(_ array: [T], isOrderedBefore: (T, T) -> Bool) -> [T] {
         var a = array
         for x in 1..<a.count {
             var y = x
             let temp = a[y]
             while y > 0 && isOrderedBefore(temp, a[y - 1]) {
-                //a.swapAt(y - 1, y)
                 a[y] = a[y - 1]
                 y -= 1
             }
+            
             a[y] = temp
         }
+        
         return a
     }
 }
--- DataStructureAndAlgorithm/main.swift
+++ DataStructureAndAlgorithm/main.swift
@@ -8,16 +8,6 @@
 
 import Foundation
 
-//Stack test
-var myStack = Stack<Any>()
-myStack.push(1)
-myStack.push(2)
-print(myStack)
-
-let insert = InsertionSort()
-let strings = [ "b", "a", "d", "c", "e" ]
-print(insert.insertionSort(strings, <))
-
 let binary = BinarySearch()
 let numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67]
 print(binary.binarySearch(numbers, key: 23, range: 0..<numbers.count)!)
