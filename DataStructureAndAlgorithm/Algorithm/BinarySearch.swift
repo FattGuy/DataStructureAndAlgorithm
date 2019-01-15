adb706cffbdaf7bf88132a4402c6c2c397dcd52c
FattGuy <fchang@myriadmobile.com>
2019-01-15 07:24:05 -0600

    added a iterative binary search

--- DataStructureAndAlgorithm/Algorithm/BinarySearch.swift
+++ DataStructureAndAlgorithm/Algorithm/BinarySearch.swift
@@ -9,6 +9,7 @@
 import Foundation
 
 class BinarySearch {
+    //Recursive
     func binarySearch<T: Comparable>(_ a: [T], key: T, range: Range<Int>) -> Int? {
         let low = range.lowerBound
         let high = range.upperBound
@@ -24,5 +25,23 @@ class BinarySearch {
                 return midIndex
             }
         }
+    }
+    
+    //Iterative
+    func binarySearchIterative<T: Comparable>(_ a: [T], key: T) -> Int? {
+        var low = 0
+        var high = a.count
+        let midIndex = low + (high - low) / 2
+        while low < high {
+            if a[midIndex] == key {
+                return midIndex
+            } else if a[midIndex] > key {
+                high = midIndex
+            } else {
+                low = midIndex + 1
+            }
+        }
+        
+        return nil
     }
 }
