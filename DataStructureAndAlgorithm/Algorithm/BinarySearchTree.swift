2aa8bd6889ad8ccaeb2e9d08db834b9416ea1c6e
FattGuy <fchang@myriadmobile.com>
2019-01-15 08:14:58 -0600

    rewrite and update the binary search tree

--- DataStructureAndAlgorithm/Algorithm/BinarySearchTree.swift
+++ DataStructureAndAlgorithm/Algorithm/BinarySearchTree.swift
@@ -8,31 +8,117 @@
 
 import Foundation
 
-public class BinarySearchTree<T: Comparable> {
-    private(set) public var value: T
-    private(set) public var parent: BinarySearchTree?
-    private(set) public var left: BinarySearchTree?
-    private(set) public var right: BinarySearchTree?
+enum BinaryTree<T: Comparable> {
+    case empty
+    indirect case node(BinaryTree, T, BinaryTree)
     
-    public init(value: T) {
-        self.value = value
+    var count: Int {
+        switch self {
+        case let .node(left, _, right):
+            return left.count + 1 + right.count
+        case .empty:
+            return 0
+        }
     }
     
-    public var isRoot: Bool {
-        return parent == nil
+    // 1.
+    mutating func naiveInsert(newValue: T) {
+        // 2.
+        guard case .node(var left, let value, var right) = self else {
+            // 3.
+            self = .node(.empty, newValue, .empty)
+            return
+        }
+        
+        // 4. TODO: Implement rest of algorithm!
+        if newValue > value {
+            right.naiveInsert(newValue: newValue)
+        } else {
+            left.naiveInsert(newValue: newValue)
+        }
     }
     
-    public var isLeaf: Bool {
-        return left == nil && right == nil
+    mutating func insert(newValue: T) {
+        self = newTreeWithInsertedValue(newValue: newValue)
     }
     
-    public var isLeftChild: Bool {
-        return parent?.left === self
+    private func newTreeWithInsertedValue(newValue: T) -> BinaryTree {
+        switch self {
+        // 1
+        case .empty:
+            return .node(.empty, newValue, .empty)
+        // 2
+        case let .node(left, value, right):
+            if newValue < value {
+                return .node(left.newTreeWithInsertedValue(newValue: newValue), value, right)
+            } else {
+                return .node(left, value, right.newTreeWithInsertedValue(newValue: newValue))
+            }
+        }
     }
     
-    public var isRightChild: Bool {
-        return parent?.right === self
+    func traverseInOrder(process: (T) -> ()) {
+        switch self {
+        // 1
+        case .empty:
+            return
+        // 2
+        case let .node(left, value, right):
+            left.traverseInOrder(process: process)
+            process(value)
+            right.traverseInOrder(process: process)
+        }
     }
     
+    func traversePreOrder( process: (T) -> ()) {
+        switch self {
+        case .empty:
+            return
+        case let .node(left, value, right):
+            process(value)
+            left.traversePreOrder(process: process)
+            right.traversePreOrder(process: process)
+        }
+    }
     
+    func traversePostOrder( process: (T) -> ()) {
+        switch self {
+        case .empty:
+            return
+        case let .node(left, value, right):
+            left.traversePostOrder(process: process)
+            right.traversePostOrder(process: process)
+            process(value)
+        }
+    }
+    
+    func search(searchValue: T) -> BinaryTree? {
+        switch self {
+        case .empty:
+            return nil
+        case let .node(left, value, right):
+            // 1
+            if searchValue == value {
+                return self
+            }
+            
+            // 2
+            if searchValue < value {
+                return left.search(searchValue: searchValue)
+            } else {
+                return right.search(searchValue: searchValue)
+            }
+        }
+    }
+}
+
+extension BinaryTree: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case let .node(left, value, right):
+            return "value: \(value), left = [" + left.description + "], right = [" + right.description + "]"
+        case .empty:
+            return ""
+        }
+    }
 }
