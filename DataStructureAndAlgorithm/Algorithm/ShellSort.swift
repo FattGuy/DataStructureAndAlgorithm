d1c0c3c85220c804f1414703e7c6ab6a793793f1
FattGuy <chang809@umn.edu>
2019-02-26 20:16:24 -0600

    updated shell sort code

--- DataStructureAndAlgorithm/Algorithm/ShellSort.swift
+++ DataStructureAndAlgorithm/Algorithm/ShellSort.swift
@@ -9,21 +9,27 @@
 import Foundation
 
 class ShellSort {
-    func shellsort<T>(seq: inout [T]) where T : Comparable {
-        var inc = seq.count / 2
-        while inc > 0 {
-            for (var i, el) in seq.enumerated() {
-                while i >= inc && seq[i - inc] > el {
-                    seq[i] = seq[i - inc]
-                    i -= inc
+    public func shellSort(_ list: inout [Int]) {
+        
+        var sublistCount = list.count / 2
+        
+        while sublistCount > 0 {
+            
+            for index in 0..<list.count {
+                
+                guard index + sublistCount < list.count else { break }
+                
+                if list[index] > list[index + sublistCount] {
+                    swap(&list[index], &list[index + sublistCount])
                 }
-                seq[i] = el
+                
+                guard sublistCount == 1 && index > 0 else { continue }
+                
+                if list[index - 1] > list[index] {
+                    swap(&list[index - 1], &list[index])
+                }
             }
-            if inc == 2 {
-                inc = 1
-            } else {
-                inc = inc * 5 / 11
-            }
+            sublistCount = sublistCount / 2
         }
     }
 }
