76eb7cc84ff1ce4f2fe91f2f55ea3732a923a008
FattGuy <chang809@umn.edu>
2019-10-27 14:37:13 -0500

    add new funcs

---
+++ DataStructureAndAlgorithm/Grokking/SumFunc.swift
@@ -0,0 +1,16 @@
+//
+//  SumFunc.swift
+//  DataStructureAndAlgorithm
+//
+//  Created by Feng Chang on 10/27/19.
+//  Copyright © 2019 Feng Chang. All rights reserved.
+//
+
+//For excercise 4.1
+import Foundation
+
+class SumFunc {
+    func sum() {
+        
+    }
+}
